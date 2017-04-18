0a32c653b2b826534f12261ddca7e911cf7072a2
Janus Nicon <janusnic@gmail.com>
2017-04-18 15:44:28 +0300

    Node start

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 var mongoose = require('mongoose'); 					// mongoose for mongodb
-var port  	 = process.env.PORT || 5000; 				// set the port
+var port  	 = process.env.PORT || 3000; 				// set the port
 var database = require('./config/database'); 			// load the database config
 var morgan   = require('morgan');
 var bodyParser = require('body-parser');
@@ -10,7 +10,7 @@ var methodOverride = require('method-override');
 // configuration ===============================================================
 mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,1 @@
-angular.module('scotchTodo', ['todoController', 'todoService']);
+angular.module('janusTodo', ['todoController', 'todoService']);
